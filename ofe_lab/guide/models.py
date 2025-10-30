848f007201a11ce37f721c487cbc40c5295e4b97
Dima <kocheshkovds@gmail.com>
2025-10-30 19:26:59 +0300

    модели для статей справочника

--- ofe_lab/guide/models.py
+++ ofe_lab/guide/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+    name = models.CharField(max_length=200, verbose_name='раздел')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Раздел спавочника'
+        verbose_name_plural = 'Разделы справочника'
+
+class Article(models.Model):
+    section = models.ForeignKey(Section, verbose_name='раздел', on_delete=models.CASCADE, related_name='articles')
+    priority = models.IntegerField(verbose_name='приоритет')
+    name = models.CharField(max_length=200, verbose_name='название')
+    path_to_file = models.CharField(max_length=200, verbose_name='путь к файлу')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
